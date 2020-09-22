412811f9e050a7a5e509e9c133e70bc69b1ebeff
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-09-22 15:07:07 +0530

    [Prerna]ADDED: Employee Present or Absent functionality

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,7 +3,7 @@ public class EmpWageBuilder
 
 	public static void main( String args[])
 	{
-		
+		System.out.println("Welcome to Employee Wage Computation Problem");
 		int IS_FULL_TIME= 1;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if(empCheck == IS_FULL_TIME)
