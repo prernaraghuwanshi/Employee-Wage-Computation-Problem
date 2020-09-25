91f712b855d2f36c033ef0442d05a7120997f6a0
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-09-25 16:27:59 +0530

    [Prerna]ADDED: Using instance variables and computing total wage for each company

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,8 +2,21 @@ public class EmpWageBuilder
 {
 	public static final int IS_FULL_TIME= 2;
 	public static final int IS_PART_TIME = 1;
-	
-	public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHrsPerMonth)
+
+	private final String company;
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxHrsPerMonth;
+	private int totalEmpWage;
+
+	public EmpWageBuilder(String company, int empRatePerHour, int numOfWorkingDays, int maxHrsPerMonth)
+	{
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHrsPerMonth = maxHrsPerMonth;
+	}
+	public void computeEmpWage()
 	{
 		int empHrs = 0;
 		int totalEmpHrs = 0;
@@ -26,17 +39,26 @@ public class EmpWageBuilder
 			totalEmpHrs += empHrs;
 			System.out.println("Day#: "+ totalWorkingDays + "Emp Hr: "+empHrs);
 		}
-		int totalEmpWage = totalEmpHrs * empRatePerHour;
-		System.out.println("Total Emp Wage for Company: "+company+" is: "+totalEmpWage);
-		return totalEmpWage;
+		totalEmpWage = totalEmpHrs * empRatePerHour;
+		//System.out.println("Total Emp Wage for Company: "+company+" is: "+totalEmpWage);
+		//return totalEmpWage;
+	}
+	@Override
+	public String toString()
+	{
+		return "Total Emp Wage for Company "+company+" is: "+totalEmpWage;
 	}
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to Employee Wage Computation Problem");
-		computeEmpWage("DMart",20,2,10);
-		computeEmpWage("Reliance",10,4,20);
+		EmpWageBuilder dMart = new EmpWageBuilder("DMart", 20, 2, 10);
+		EmpWageBuilder reliance = new EmpWageBuilder("Reliance", 10, 4, 20);
+		dMart.computeEmpWage();
+		System.out.println(dMart);
+		reliance.computeEmpWage();
+		System.out.println(reliance);
 	}
-	
 
+		
 
 }
