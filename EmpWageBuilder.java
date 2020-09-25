f370ce44782faf9460368d71307d88fa7861f514
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-09-25 16:21:24 +0530

    [Prerna]ADDED: Class method with functions for multiple companies

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,15 +2,13 @@ public class EmpWageBuilder
 {
 	public static final int IS_FULL_TIME= 2;
 	public static final int IS_PART_TIME = 1;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
-	public static int computeEmpWage()
+	
+	public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHrsPerMonth)
 	{
 		int empHrs = 0;
 		int totalEmpHrs = 0;
 		int totalWorkingDays = 0;
-		while(totalEmpHrs<= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+		while(totalEmpHrs<= maxHrsPerMonth && totalWorkingDays < numOfWorkingDays)
 		{
 			totalWorkingDays++;			
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -28,15 +26,17 @@ public class EmpWageBuilder
 			totalEmpHrs += empHrs;
 			System.out.println("Day#: "+ totalWorkingDays + "Emp Hr: "+empHrs);
 		}
-		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Total Emp Wage: "+ totalEmpWage);
+		int totalEmpWage = totalEmpHrs * empRatePerHour;
+		System.out.println("Total Emp Wage for Company: "+company+" is: "+totalEmpWage);
 		return totalEmpWage;
 	}
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to Employee Wage Computation Problem");
-		computeEmpWage();
+		computeEmpWage("DMart",20,2,10);
+		computeEmpWage("Reliance",10,4,20);
 	}
+	
 
 
 }
